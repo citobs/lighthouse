72585ba1e7f08b5e8ce66f99f6692bf1d58f8391
citobs <altiorapetamus7@hanmail.net>
2024-07-30 23:38:07 +0900

    test

---
+++ lighthouserc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    ci: {
+      collect: {
+        url: ['https://www.amoremall.com/kr/ko/display/main'],
+        collect: {
+          numberOfRuns: 5,
+        },
+      },
+      upload: {        
+        target: 'temporary-public-storage',
+      },
+    },
+  }
