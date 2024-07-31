20cca30b9b6aeec885cbf3b8821a45ba67719ee7
citobs <altiorapetamus7@hanmail.net>
2024-07-31 09:06:30 +0900

    last25

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,7 +1,7 @@
 module.exports = {
     ci: {
       collect: {
-        //staticDistDir: "./build",
+        staticDistDir: "./build",
         // startServerCommand: "npm run start", // 서버를 키는 명령어를 통해서도 실행 가능
         url: ["http://localhost:3000"],
         numberOfRuns: 5,
